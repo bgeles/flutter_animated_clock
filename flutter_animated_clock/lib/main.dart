0f8155742a85f3c83e5a892c7e233f9de8a52b1f
Bruno Glauco Eles <bgeles@gmail.com>
2021-04-19 08:02:21 -0300

    add package flutter local notification

--- flutter_animated_clock/lib/main.dart
+++ flutter_animated_clock/lib/main.dart
@@ -1,11 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animated_clock/enums.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 
 import 'views/homepage.dart';
 import 'model/menu_info.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  var initializationSettingsAndroid = AndroidInitializationSettings('codex_log');
+  var initializationSettingsIOS = IOSInitializationSettings(
+  requestAlertPermission: true,
+    requestBadgePermission: true,
+    requestSoundPermission: true,
+    onDidReceiveLocalNotification: (int id, String title, String body, String payload) async{});
+
+  var initializationSettings = InitializationSettings(initializationSettingsAndroid, initializationSettingsIOS);
+
+  await flutterLocalNotificationsPlugin.initialize(initializationSettings,onSelectNotification: (String payload) async {
+    if (payload !=null){
+      debugPrint('notification payload: ' + payload);
+    }
+  });
+
   runApp(MyApp());
 }
 
